1574e9acce19e6ac3c5277e383452184f72e3841
AhmedAlkhazragi <ahmed.alkhazragi01@gmail.com>
2022-08-17 19:19:17 +1000

    add pencil, eraser, clear functionality.

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 /** global variables */
 
-let currentPencilColor = "black";
+let currentPencilColor = String(document.querySelector(".color-picker").value);
 const pencil = document.querySelector(".pencil");
 const eraser = document.querySelector(".eraser");
 const clear = document.querySelector(".clear");
 const darken = document.querySelector(".darken");
 const lighten = document.querySelector(".lighten");
 const rgb = document.querySelector(".rgb");
+
+console.log(currentPencilColor);
 
 /** draw grid function */
 
@@ -22,7 +24,7 @@ function drawGrid(input) {
     display.style['grid-template-columns'] = `repeat(${input}, ${input}fr)`;
 }
 
-drawGrid(16);
+drawGrid(32);
 
 /** draw event-listeners */
 
@@ -34,6 +36,10 @@ document.querySelectorAll(".pixel").forEach((pixel) => {
     pixel.addEventListener("mouseup", (e) => {
         mouseIsDown = false;
     })
+})
+
+document.querySelector(".color-picker").addEventListener("mouseout", (e) => {
+    currentPencilColor = String(document.querySelector(".color-picker").value);
 })
 
 document.querySelectorAll(".pixel").forEach((pixel) => {
@@ -50,4 +56,26 @@ document.querySelectorAll(".pixel").forEach((pixel) => {
     pixel.addEventListener("click", (e) => {
         pixel.style['background-color'] = currentPencilColor;
     });
+
+    clear.addEventListener("click", (e) => {
+        pixel.style['background-color'] = "white";
+    });
+
+    eraser.addEventListener("click", (e) => {
+        pencil.style.cssText = "border: none; background-color: white; color: #38393E;";
+        darken.style.cssText = "border: none; background-color: white; color: #38393E;";
+        rgb.style.cssText = "border: none; background-color: white; color: #38393E;";
+        lighten.style.cssText = "border: none; background-color: white; color: #38393E;";
+        eraser.style.cssText = "border: 5px solid #38393E; background-color:#38393E; color: lightgray;";
+        currentPencilColor = "white";
+    });
+
+    pencil.addEventListener("click", (e) => {
+        darken.style.cssText = "border: none; background-color: white; color: #38393E;";
+        lighten.style.cssText = "border: none; background-color: white; color: #38393E;";
+        rgb.style.cssText = "border: none; background-color: white; color: #38393E;";
+        eraser.style.cssText = "border: none; background-color: white; color: #38393E;";
+        pencil.style.cssText = "border: 5px solid #38393E; background-color:#38393E; color: lightgray;";
+        currentPencilColor = String(document.querySelector(".color-picker").value);
+    })
 })
